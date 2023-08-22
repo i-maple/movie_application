44e7d595a9c1404658555c35728457e48e944581
Nishant <nishantyt0@gmail.com>
2023-08-22 15:11:58 +0545

    refac: separated url strings and fixed minor issue

--- lib/logic/service/api_service.dart
+++ lib/logic/service/api_service.dart
@@ -4,16 +4,19 @@ import 'package:flutter_dotenv/flutter_dotenv.dart' show dotenv;
 import 'package:movie_application/model/data_model.dart';
 
 class APIService {
-  String url =
-      'https://api.themoviedb.org/3/discover/movie/?api_key=${dotenv.env['API_KEY']}';
+  static String baseUrl =
+      'https://api.themoviedb.org/';
+  static String apikey = 'api_key=${dotenv.env['API_KEY']}';
+  static String discoverMovie = '/3/discover/movie/?';
+
   List<DataModel> models = [];
 
   Future<http.Response> initialize() async {
-    return await http.get(Uri.parse(url),
+    return await http.get(Uri.parse(baseUrl+discoverMovie+apikey),
         headers: {'Authorization': 'Bearer ${dotenv.env['BEARER']}'});
   }
 
-  getAllMoviesList() async {
+  Future<List<DataModel>> getAllMoviesList() async {
     http.Response res = await initialize();
     Map moviesMapsList = jsonDecode(res.body);
     for (Map maps in moviesMapsList['results']) {
