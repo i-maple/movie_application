3d7a8cfce8cbe04d099ed7f78c81b055ff337c63
Nishant <nishantyt0@gmail.com>
2023-08-22 09:27:37 +0545

    chore: added colors pallettes

---
+++ lib/ui/utils/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryBackground = Color(0xFFF4F4F4);
+  static const Color primaryText = Color(0xFF333333);
+  static const Color secondaryText = Color(0xFF666666);
+
+  static const Color accentColor = Color(0xFFFF6F61);
+  static const Color hoverColor = Color(0xFFFF9B8E);
+
+  static const Color actionButton = Color(0xFF44AEE6);
+  static const Color actionButtonText = Colors.white;
+
+  static const Color dividerLine = Color(0xFFDDDDDD);
+
+  static const Color iconColor = Color(0xFF888888);
+
+  static const Color backgroundHover = Color(0xFFE0E0E0);
+}
